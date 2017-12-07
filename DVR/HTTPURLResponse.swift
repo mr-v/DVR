003a3d53adc7ebd28a0cd41ff3298044694ddbcd
Witold Skibniewski <witold.skibniewski@gmail.com>
2017-12-07 14:43:12 +0100

    Remove HTTPURLResponse subclass
    Fixes type inference issue, that caused problem with non-200 status code responses.
    https://github.com/venmo/DVR/pull/75

--- DVR/HTTPURLResponse.swift
+++ DVR/HTTPURLResponse.swift
@@ -1,57 +1,14 @@
 import Foundation
 
-// There isn't a mutable HTTPURLResponse, so we have to make our own.
-class HTTPURLResponse: Foundation.HTTPURLResponse {
-
-    // MARK: - Properties
-
-    private var _url: URL?
-    override var url: URL? {
-        get {
-            return _url ?? super.url
-        }
-
-        set {
-            _url = newValue
-        }
-    }
-
-    private var _statusCode: Int?
-    override var statusCode: Int {
-        get {
-            return _statusCode ?? super.statusCode
-        }
-
-        set {
-            _statusCode = newValue
-        }
-    }
-
-    private var _allHeaderFields: [AnyHashable: Any]?
-    override var allHeaderFields: [AnyHashable: Any] {
-        get {
-            return _allHeaderFields ?? super.allHeaderFields
-        }
-
-        set {
-            _allHeaderFields = newValue
-        }
-    }
-}
-
-
 extension HTTPURLResponse {
-    convenience init(dictionary: [String: Any]) {
-        let url = URL(string: dictionary["url"] as! String)!
+    convenience init?(dictionary: [String: Any]) {
+        guard let urlString = dictionary["url"] as? String,
+            let url = URL(string: urlString),
+            let statusCode = dictionary["status"] as? Int
+        else { return nil }
 
-        self.init(url: url, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
-
-        if let headers = dictionary["headers"] as? [String: String] {
-            allHeaderFields = headers
-        }
-
-        if let status = dictionary["status"] as? Int {
-            statusCode = status
-        }
+        let headers = dictionary["headers"] as? [String: String]
+        self.init(url: url, statusCode: statusCode, httpVersion: nil, headerFields: headers)
     }
 }
+
--- DVR/Interaction.swift
+++ DVR/Interaction.swift
@@ -99,12 +99,14 @@ extension Interaction {
 
     init?(dictionary: [String: Any]) {
         guard let request = dictionary["request"] as? [String: Any],
-            let response = dictionary["response"] as? [String: Any],
-            let recordedAt = dictionary["recorded_at"] as? TimeInterval else { return nil }
+            let responseDict = dictionary["response"] as? [String: Any],
+            let response = HTTPURLResponse(dictionary: responseDict),
+            let recordedAt = dictionary["recorded_at"] as? TimeInterval
+        else { return nil }
 
         self.request = NSMutableURLRequest(dictionary: request) as URLRequest
-        self.response = HTTPURLResponse(dictionary: response)
+        self.response = response
         self.recordedAt = Date(timeIntervalSince1970: recordedAt)
-        self.responseData = Interaction.dencodeBody(response["body"], headers: response["headers"] as? [String: String])
+        self.responseData = Interaction.dencodeBody(responseDict["body"], headers: responseDict["headers"] as? [String: String])
     }
 }
